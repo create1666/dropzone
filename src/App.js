f02fc7ba74ec7587e8d3e384098b767aded21497
create1666 <create1666@gmail.com>
2021-04-26 21:00:56 +0100

    simple drag and drop component initialization stage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Dropzone from "./Dropzone";
+import React from 'react'
+import "./App.scss";
 
 function App() {
+  const [files, setFiles] = React.useState([])
+  const [preview, setPreview] = React.useState([])
+  console.log(files)
+  console.log(preview)
+  const onDragStart = (e) => {
+    console.log(e.dataTransfer);
+
+    e.dataTransfer.setData("text", e.target.id);
+    console.log("Drag start");
+  };
+
+  const onDragEnd = (e) => {
+    console.log("Draga end");
+  };
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const onDrop = (e) => {
+    e.preventDefault();
+    console.log(e.dataTransfer.files);
+    const newFiles = e.dataTransfer.files
+    setFiles(files.concat(...newFiles)) // concat(...[1,2,3,4]) -> concat(1,2,3,4)
+
+    const id = e.dataTransfer.getData("text");
+    const element = document.getElementById(id);
+    e.target.appendChild(element);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="App">
+        <p
+          id="drag-p"
+          draggable={true}
+          onDragStart={onDragStart}
+          onDragEnd={onDragEnd}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          We will be dargging this very soon
+        </p>
+      </div>
+      <div onDrop={onDrop} onDragOver={onDragOver}>
+        A new div
+      </div>
+      <Dropzone files={files} onDrop={onDrop} onDragOver={onDragOver} />
+      <div className='delete-item'>Drag here to delete</div>
+    </>
   );
 }
 
