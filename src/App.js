c583203aa0eb743ab954973021b5686dce75f89c
create1666 <create1666@gmail.com>
2021-04-27 05:02:11 +0100

    initializing the dropzone component - focusing on the functio

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
-import logo from "./logo.svg";
-import Dropzone from "./Dropzone";
-import React from 'react'
-import "./App.scss";
+import React from "react";
+import DropZone from "./Dropzone";
+import "./App.scss"; // install node-sass to use scss
 
 function App() {
-  const [files, setFiles] = React.useState([])
-  const [preview, setPreview] = React.useState([])
-  console.log(files)
-  console.log(preview)
-  const onDragStart = (e) => {
-    console.log(e.dataTransfer);
 
-    e.dataTransfer.setData("text", e.target.id);
-    console.log("Drag start");
-  };
+  const [files, setFiles] = useState([])
+  const onDragOver = () => {
+    e.preventDefault()
+  }
 
-  const onDragEnd = (e) => {
-    console.log("Draga end");
-  };
+  const onDrop = () => {
+    e.preventDefault()
+    const newfileList=e.dataTranfer.files
+    setFiles([...files, newfileList ])
+  }
 
-  const onDragOver = (e) => {
-    e.preventDefault();
-  };
-
-  const onDrop = (e) => {
-    e.preventDefault();
-    console.log(e.dataTransfer.files);
-    const newFiles = e.dataTransfer.files
-    setFiles(files.concat(...newFiles)) // concat(...[1,2,3,4]) -> concat(1,2,3,4)
-
-    const id = e.dataTransfer.getData("text");
-    const element = document.getElementById(id);
-    e.target.appendChild(element);
-  };
-  return (
-    <>
-      <div className="App">
-        <p
-          id="drag-p"
-          draggable={true}
-          onDragStart={onDragStart}
-          onDragEnd={onDragEnd}
-        >
-          We will be dargging this very soon
-        </p>
-      </div>
-      <div onDrop={onDrop} onDragOver={onDragOver}>
-        A new div
-      </div>
-      <Dropzone files={files} onDrop={onDrop} onDragOver={onDragOver} />
-      <div className='delete-item'>Drag here to delete</div>
-    </>
-  );
+  
+  return <div className={App}>
+    <h1>Files-Image Drag and drop feature</h1>
+    <DropZone/>
+  </div>
 }
-
 export default App;
+
